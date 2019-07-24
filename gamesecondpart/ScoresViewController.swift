aedfec3c841a9e6f498a938a614120d0b2d76cf9
lalitmanwani <lalit631995@gmail.com>
2019-07-24 19:58:58 -0400

    Update
    Added Table View

---
+++ gamesecondpart/ScoresViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ScoresViewController.swift
+//  gamesecondpart
+//
+//  Created by Saket Chinchalikar on 7/24/19.
+//  Copyright © 2019 Lalit. All rights reserved.
+//
+
+import UIKit
+
+class ScoresViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
